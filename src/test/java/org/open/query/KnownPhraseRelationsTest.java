1263be0d53b1f77757bd9558ed5ce2d597da9689
Dhanushanth <root@dhanushanth-HP-ProBook-4430s>
2014-08-26 11:28:20 +0530

    Added Apache Stopwatch for performace Analysis

--- src/test/java/org/open/query/KnownPhraseRelationsTest.java
+++ src/test/java/org/open/query/KnownPhraseRelationsTest.java
@@ -3,14 +3,16 @@ package org.open.query;
 import java.io.IOException;
 import java.util.Collection;
 
+import org.apache.commons.lang.time.StopWatch;
 import org.open.query.knownphrases.RelatedQueryGenertor;
 
 public class KnownPhraseRelationsTest {
-
+	
 	public static void main(String[] args) throws IOException {
 		RelatedQueryGenertor kpr = new RelatedQueryGenertor();
-		long start = System.currentTimeMillis();
-		String per = "abdul kalam";
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		String per = "osama";
 		String con = "india";
 		String jo  = "engineer";
 		String cit = "bangalore";
@@ -19,14 +21,14 @@ public class KnownPhraseRelationsTest {
 		Collection<String> country = kpr.getRelations(con);
 		Collection<String> job = kpr.getRelations(jo);
 		Collection<String> city = kpr.getRelations(cit);
-		long end = System.currentTimeMillis();
+		stopWatch.stop();
 		
 		System.out.println("\nCheck Person : "+ per +"\n"+ "Total result : "+ person.size() +"\n"+ "Relation : "+ person);
 		System.out.println("\nCheck Country : "+ con +"\n"+ "Total result : "+ country.size() +"\n"+ "Relation : "+ country);
 		System.out.println("\nCheck Job : "+jo+"\n"+ "Total result : "+ job.size() +"\n"+ "Relation : "+ job);
 		System.out.println("\nCheck City : "+cit+"\n"+ "Total result : "+ city.size() +"\n"+ "Relation : "+ city);
 		
-		System.out.println("\nDuration : " + (end- start));
+		System.out.println("\nDuration : " + stopWatch.getTime());
 	}
 
 }
