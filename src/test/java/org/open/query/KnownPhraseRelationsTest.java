dad91c8372293bc86d63e70990e4de72d1dc38d8
Dhanushanth <root@dhanushanth-HP-ProBook-4430s>
2014-08-26 17:41:45 +0530

    Added Prefix and Test for Animals and birds.

--- src/test/java/org/open/query/KnownPhraseRelationsTest.java
+++ src/test/java/org/open/query/KnownPhraseRelationsTest.java
@@ -12,21 +12,27 @@ public class KnownPhraseRelationsTest {
 		RelatedQueryGenertor kpr = new RelatedQueryGenertor();
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
-		String per = "osama";
+		String per = "abdul kalam";
 		String con = "india";
 		String jo  = "engineer";
 		String cit = "bangalore";
+		String ani = "Cat";
+		String bir = "Cormorants";
 		
 		Collection<String> person = kpr.getRelations(per);
 		Collection<String> country = kpr.getRelations(con);
 		Collection<String> job = kpr.getRelations(jo);
 		Collection<String> city = kpr.getRelations(cit);
+		Collection<String> animal = kpr.getRelations(ani);
+		Collection<String> bird = kpr.getRelations(bir);
 		stopWatch.stop();
 		
 		System.out.println("\nCheck Person : "+ per +"\n"+ "Total result : "+ person.size() +"\n"+ "Relation : "+ person);
 		System.out.println("\nCheck Country : "+ con +"\n"+ "Total result : "+ country.size() +"\n"+ "Relation : "+ country);
 		System.out.println("\nCheck Job : "+jo+"\n"+ "Total result : "+ job.size() +"\n"+ "Relation : "+ job);
 		System.out.println("\nCheck City : "+cit+"\n"+ "Total result : "+ city.size() +"\n"+ "Relation : "+ city);
+		System.out.println("\nCheck Animal : "+ani+"\n"+ "Total result : "+ animal.size() +"\n"+ "Relation : "+ animal);
+		System.out.println("\nCheck Bird : "+bir+"\n"+ "Total result : "+ bird.size() +"\n"+ "Relation : "+ bird);
 		
 		System.out.println("\nDuration : " + stopWatch.getTime());
 	}
